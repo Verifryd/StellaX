bee46cc946660bb7acf5d03da88016b022493345
Verifryd <verifryd.video@gmail.com>
2023-08-11 17:59:37 -0400

    update

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
+// Initialization code
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Cube code
 var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
-
+// OrbitControls code
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-function animate() {
-    requestAnimationFrame(animate);
+camera.position.z = 5;
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+var animate = function () {
+  requestAnimationFrame(animate);
 
-    controls.update();
+  // Cube rotation
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
-}
+  // Update controls
+  controls.update();
+
+  renderer.render(scene, camera);
+};
 
 animate();
