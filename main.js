251cf27d9b5600a5b9f0aa1c86c028a80e4d3e15
Verifryd <verifryd.video@gmail.com>
2023-08-11 12:12:12 -0400

    update

---
+++ main.js
@@ -0,0 +1,8 @@
+import * as THREE from 'https://threejs.org/build/three.module.js';
+import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
+
+// Your Three.js code here
+// ...
+
+// Initialize controls
+var controls = new OrbitControls(camera, renderer.domElement);
