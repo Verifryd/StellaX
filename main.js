37aeb4ecbabf92cc7c3f0bf4a812a0fe51728694
Verifryd <verifryd.video@gmail.com>
2023-08-11 16:57:50 -0400

    update

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-import * as THREE from "../node_modules/three/build/three.module.js";
-import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
-
 // Create scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
