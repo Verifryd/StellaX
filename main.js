527e8e01a125f6f9c74971701dde623e38f50c73
Verifryd <verifryd.video@gmail.com>
2023-08-11 17:17:42 -0400

    update

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
-import * as THREE from "../node_modules/three/build/three.module.js";
-import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-// Create scene, camera, and renderer
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('scene-container').appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
-// Create cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// Position camera
 camera.position.z = 5;
 
-// Create controls
-const controls = new OrbitControls(camera, renderer.domElement);
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-// Animation loop
 function animate() {
     requestAnimationFrame(animate);
+
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-    controls.update(); // Required for controls to work
+
+    controls.update();
+
     renderer.render(scene, camera);
 }
 
 animate();
-
