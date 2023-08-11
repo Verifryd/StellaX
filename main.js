ae20ca8e8c0ef2dd1360310f1ccde749b4ff22bf
Verifryd <verifryd.video@gmail.com>
2023-08-11 12:23:09 -0400

    update

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 import * as THREE from 'https://threejs.org/build/three.module.js';
 import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 
-// Your Three.js code here
-// ...
+// Create scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// Initialize controls
-var controls = new OrbitControls(camera, renderer.domElement);
+// Add cube
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Add controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Set camera position
+camera.position.z = 5;
+
+// Animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+animate();
