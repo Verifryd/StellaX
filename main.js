909c59de9ab7624cfb0eaa3b31049a2f4bfc2658
Verifryd <verifryd.video@gmail.com>
2023-08-11 16:14:14 -0400

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://threejs.org/build/three.module.js';
+import * as THREE from "../node_modules/three/build/three.module.js';
 import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 
 // Create scene, camera, and renderer
