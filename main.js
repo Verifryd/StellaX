0442aca6e0c5aab7ad0da33bcb4ab6b696b32d1f
Verifryd <verifryd.video@gmail.com>
2023-08-11 18:39:45 -0400

    update

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
-
+// Initialization code
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Cube code
 var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+// OrbitControls code
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
 camera.position.z = 5;
 
-var controls = new OrbitControls(camera, renderer.domElement);
-
-function animate() {
+var animate = function () {
   requestAnimationFrame(animate);
+
+  // Cube rotation
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+
+  // Update controls
+  controls.update();
+
   renderer.render(scene, camera);
-}
+};
 
 animate();
