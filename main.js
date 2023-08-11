3d2e616b2c14eff3d7b30f7cbbc9278fd8bb6846
Verifryd <verifryd.video@gmail.com>
2023-08-11 16:07:44 -0400

    update

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// No need to import THREE or OrbitControls here
+import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 
+// Your existing code for setting up THREE.js scene
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -14,14 +14,16 @@ scene.add(cube);
 
 camera.position.z = 5;
 
-// Initialize OrbitControls here
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
+// Add OrbitControls
+var controls = new OrbitControls(camera, renderer.domElement);
 
 var animate = function () {
     requestAnimationFrame(animate);
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    controls.update();
 
     renderer.render(scene, camera);
 };
