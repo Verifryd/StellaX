fb10f26d403fd1e706fb99a656dd5a312c3a4b61
Verifryd <verifryd.video@gmail.com>
2023-08-16 20:42:20 -0400

    update

--- scripts.js
+++ scripts.js
@@ -1,60 +1,71 @@
-// Function to handle the animated cutscene (optional view or one-time display)
-// Function to handle the animated cutscene (optional view or one-time display)
-function handleCutscene() {
-  var introAnimation = document.getElementById('intro-animation');
-  var buttonsContainer = document.getElementById('buttons-container');
+var video = document.getElementById('stellax-video');
 
-  // When the video ends
-  introAnimation.addEventListener('ended', function() {
-    // Hide the video
-    introAnimation.style.display = 'none';
+// Function to handle the video click
+function handleVideoClick() {
+  // Play the video
+  video.play();
 
-    // Show the buttons
-    buttonsContainer.style.display = 'flex';
-  });
+  // Open Manifold connect widget
+  window.manifold.open();
 }
 
-// Function to handle the pixel split transition
-function handlePixelSplit() {
-  var pixelVideo = document.getElementById('stellaX-pixel');
-
-  // Code to create the pixel splitting animation (e.g., using CSS animations or JavaScript)
-
-  // Code to handle the transition to different pages based on user interaction with the pixels
+// Function to handle the wallet connection
+function handleWalletConnection() {
+  // Pause the video
+  video.pause();
+  
+  // Resume playing the video
+  video.play();
 }
 
-// Add event listeners to trigger the above functions when appropriate
-window.addEventListener('load', handleCutscene);
+// Function to handle the video end
+function handleVideoEnd() {
+  // Redirect to the homepage
+  window.location.href = 'homepage.html';
+ 
 
-  var video = document.getElementById('intro-animation');
-  var buttonsContainer = document.getElementById('buttons-container');
+// Add event listeners
+video.addEventListener('click', handleVideoClick);
+video.addEventListener('ended', handleVideoEnd);
 
-  // When the video ends
-  video.addEventListener('ended', function() {
-    // Hide the video
-    video.style.display = 'none';
+// Manifold event listener for wallet connection
+window.manifold.addEventListener('walletConnected', handleWalletConnection);
 
-    // Show the buttons
-    buttonsContainer.style.display = 'flex';
-  });
+}
 
+// When the video ends
+video.addEventListener('ended', function () {
+  // Hide the video
+  video.style.display = 'none';
 
+  // Show the buttons
+  buttonsContainer.style.display = 'flex';
+});
+
+// Event listener for the button click
+document.getElementById('button1').addEventListener('click', function () {
+  // Redirect to the homepage
+  window.location.href = 'homepage.html';
+});
 
 
   /* Check if the cutscene has been viewed before (using local storage)
   if (localStorage.getItem('cutsceneViewed') !== 'true') {
     // Play the cutscene
     introAnimation.play();
+});
 
     // Mark the cutscene as viewed
     localStorage.setItem('cutsceneViewed', 'true');
   } else {
     // Optionally, hide or skip the cutscene if viewed before
     introAnimation.style.display = 'none'; */
+});
 
 // Function to handle the pixel split transition
 function handlePixelSplit() {
   var pixelVideo = document.getElementById('stellaX-pixel');
+});
 
   // Code to create the pixel splitting animation (e.g., using CSS animations or JavaScript)
 
@@ -66,5 +77,4 @@ window.addEventListener('load', handleCutscene);
 
 // Call the handleCutscene function when the page loads
 window.addEventListener('load', handleCutscene);
-
-}
+});
