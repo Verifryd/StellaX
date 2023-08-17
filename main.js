07ee05ad6714d44c372293e4914263d86de6aa2f
Verifryd <verifryd.video@gmail.com>
2023-08-17 16:22:22 -0400

    update

--- main.js
+++ main.js
@@ -1,39 +1,30 @@
-import * as THREE from 'three';
+// Get the container
+const container = document.getElementById('threejs-container');
 
-// Create a scene
+// Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, container.offsetWidth / container.offsetHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(container.offsetWidth, container.offsetHeight);
+container.appendChild(renderer.domElement);
 
-// Create a camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+// Create two cubes
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube1 = new THREE.Mesh(geometry, material);
+const cube2 = new THREE.Mesh(geometry, material);
+cube2.position.x = 2; // Position the second cube to the right
+scene.add(cube1, cube2);
+
+// Set up orbit controls
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// Position the camera
 camera.position.z = 5;
 
-// Create a renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Create a geometry
-const geometry = new THREE.BoxGeometry();
-
-// Create a material
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-// Create a mesh
-const cube = new THREE.Mesh(geometry, material);
-
-// Add the mesh to the scene
-scene.add(cube);
-
-// Animation loop
+// Render loop
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
