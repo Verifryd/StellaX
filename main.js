20765052b6e5d12cc3b1abaaf6ca53130d982f4e
Verifryd <verifryd.video@gmail.com>
2023-08-17 18:21:08 -0400

    update

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-import * as THREE from "../node_modules/three/build/three.module.js
-import { OrbitControls } from '/lib/OrbitControls.js';
-
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Create a scene
-const scene = new THREE.Scene();
+var scene = new THREE.Scene();
 
 // Create a camera
-const camera = new THREE.PerspectiveCamera(
+var camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
@@ -15,18 +14,18 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 5;
 
 // Create a renderer
-const renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Create a geometry
-const geometry = new THREE.BoxGeometry();
+var geometry = new THREE.BoxGeometry();
 
 // Create a material
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
 // Create a mesh
-const cube = new THREE.Mesh(geometry, material);
+var cube = new THREE.Mesh(geometry, material);
 
 // Add the mesh to the scene
 scene.add(cube);
