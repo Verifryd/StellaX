a05f9683cd19001a561bbe42e2813cb8a6b3ee4f
Verifryd <verifryd.video@gmail.com>
2023-08-17 18:28:29 -0400

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'node_modules/three/build/three.module.js';
-import { OrbitControls } from 'node_modules/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
 
 // Create a scene
 var scene = new THREE.Scene();
