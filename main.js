948d3a02e4e8c7bcc7172914b717d147b5b4adf4
Verifryd <verifryd.video@gmail.com>
2023-08-18 15:08:37 -0400

    update

--- main.js
+++ main.js
@@ -6,44 +6,50 @@ function initThreeJs() {
 }
 
 export function startThreeJsAnimation() {
-    initThreeJs();
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff); // White background
+
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 5;
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // First cube
+  const geometry1 = new THREE.BoxGeometry();
+  const material1 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube1 = new THREE.Mesh(geometry1, material1);
+  scene.add(cube1);
+  cube1.position.x = -2; // Positioning the first cube to the left
+
+  // Second cube
+  const geometry2 = new THREE.BoxGeometry();
+  const material2 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const cube2 = new THREE.Mesh(geometry2, material2);
+  scene.add(cube2);
+  cube2.position.x = 2; // Positioning the second cube to the right
+
+  // Orbit controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  const animate = function () {
+    requestAnimationFrame(animate);
+
+    cube1.rotation.x += 0.01;
+    cube1.rotation.y += 0.01;
+
+    cube2.rotation.x += 0.01;
+    cube2.rotation.y += 0.01;
+
+    controls.update();
+
+    renderer.render(scene, camera);
+  };
+
+  animate();
+
+  // Event listeners for cubes
+  // TODO: Add click event listeners to navigate to respective pages
 }
-
-// Create a scene
-var scene = new THREE.Scene();
-
-// Create a camera
-var camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 5;
-
-// Create a renderer
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Create a geometry
-var geometry = new THREE.BoxGeometry();
-
-// Create a material
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-// Create a mesh
-var cube = new THREE.Mesh(geometry, material);
-
-// Add the mesh to the scene
-scene.add(cube);
-
-// Animation loop
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-}
-animate();
 
