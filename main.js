a86a9e093b6b1533b9f2db48caa793455d970ef2
Verifryd <verifryd.video@gmail.com>
2023-08-18 16:25:06 -0400

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } form 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 export function startThreeJsAnimation() {
   const scene = new THREE.Scene();
