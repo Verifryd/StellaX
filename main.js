fe70801a8412d3bc6cc53b3995154da431a6ddd4
Verifryd <verifryd.video@gmail.com>
2023-08-27 15:19:13 -0400

    update

--- main.js
+++ main.js
@@ -1,73 +1,52 @@
 import * as THREE from 'three';
-import { OrbitControls } from './lib/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-function startThreeJsAnimation() {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    const renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0xffffff, 0);
-    document.getElementById('threejs-container').appendChild(renderer.domElement);
+export function startThreeJsAnimation() {
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff); // White background
 
-    const textureLoader = new THREE.TextureLoader();
-    const texture = textureLoader.load('Assets/HomepagePlaceholder.png');
-    const material = new THREE.MeshBasicMaterial({ map: texture });
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 5;
 
-    const geometry = new THREE.BoxGeometry();
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-    const cube1 = new THREE.Mesh(geometry, material);
-    const cube2 = new THREE.Mesh(geometry, material);
+  // First cube
+  const geometry1 = new THREE.BoxGeometry();
+  const material1 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube1 = new THREE.Mesh(geometry1, material1);
+  scene.add(cube1);
+  cube1.position.x = -2; // Positioning the first cube to the left
 
-    cube1.position.x = -2;
-    cube2.position.x = 2;
+  // Second cube
+  const geometry2 = new THREE.BoxGeometry();
+  const material2 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const cube2 = new THREE.Mesh(geometry2, material2);
+  scene.add(cube2);
+  cube2.position.x = 2; // Positioning the second cube to the right
 
-    scene.add(cube1);
-    scene.add(cube2);
+  // Orbit controls
+  const controls = new OrbitControls(camera, renderer.domElement);
 
-    camera.position.z = 5;
+  const animate = function () {
+    requestAnimationFrame(animate);
 
-    const raycaster = new THREE.Raycaster();
-    const mouse = new THREE.Vector2();
+    cube1.rotation.x += 0.01;
+    cube1.rotation.y += 0.01;
 
-    function onMouseClick(event) {
-        event.preventDefault();
+    cube2.rotation.x += 0.01;
+    cube2.rotation.y += 0.01;
 
-        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    controls.update();
 
-        raycaster.setFromCamera(mouse, camera);
+    renderer.render(scene, camera);
+  };
 
-        const intersects = raycaster.intersectObjects(scene.children);
+  animate();
 
-        if (intersects.length > 0) {
-            if (intersects[0].object === cube1) {
-                window.location.href = "game-overview.html";
-            } else if (intersects[0].object === cube2) {
-                window.location.href = "community-puzzles.html";
-            }
-        }
-    }
-
-    window.addEventListener('click', onMouseClick, false);
-
-    const animate = function () {
-        requestAnimationFrame(animate);
-
-        cube1.rotation.x += 0.01;
-        cube1.rotation.y += 0.01;
-
-        cube2.rotation.x += 0.01;
-        cube2.rotation.y += 0.01;
-
-        renderer.render(scene, camera);
-    };
-
-    animate();
+  // Event listeners for cubes
+  // TODO: Add click event listeners to navigate to respective pages
 }
 
-document.getElementById('intro-animation').addEventListener('ended', function() {
-    document.body.classList.add('threejs-shown');
-    document.getElementById('threejs-container').style.display = 'block';
-    startThreeJsAnimation();
-});
