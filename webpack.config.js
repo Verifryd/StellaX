2ee904cbd83111132c451da63d1d2ac2bd2abc48
Verifryd <verifryd.video@gmail.com>
2023-08-27 15:38:07 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,10 @@ const path = require('path');
 
 module.exports = {
   mode: 'development', // Set the mode to 'production' or 'development'
-  entry: 'main.js', // Make sure this path is correct
+  entry: './main.js', // Make sure this path is correct
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
   },
 };
 
