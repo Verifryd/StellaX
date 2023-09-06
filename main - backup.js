339b771d9cf28bb27b38c35b2cccf708ade2cfc4
Verifryd <verifryd.video@gmail.com>
2023-09-06 18:54:23 -0400

    update

---
+++ main - backup.js
@@ -0,0 +1,130 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+window.startThreeJsAnimation = function() {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.z = 10;
+
+        const renderer = new THREE.WebGLRenderer( { alpha: true } );
+        renderer.setClearColor(0x000000, 0);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById('threejs-container').appendChild(renderer.domElement);
+
+        const textureLoader = new THREE.TextureLoader();
+        const texture = textureLoader.load('Assets/HomepagePlaceholder.png');
+        const material = new THREE.MeshBasicMaterial({ map: texture });
+
+        const geometry = new THREE.BoxGeometry();
+        const cube1 = new THREE.Mesh(geometry, material);
+        const cube2 = new THREE.Mesh(geometry, material);
+// Raycaster and mouse vector for selecting cubes
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+let selectedCube = null;
+let isDragging = false;
+
+// Mouse down event
+window.addEventListener('mousedown', (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects([cube1, cube2]);
+
+    if (intersects.length > 0) {
+        selectedCube = intersects[0].object;
+        isDragging = true;
+    }
+});
+
+// Mouse move event
+window.addEventListener('mousemove', (event) => {
+    if (isDragging && selectedCube) {
+        const rotationSpeed = 0.01; // Adjust this value for desired rotation speed
+
+        selectedCube.rotation.x += event.movementY * rotationSpeed;
+        selectedCube.rotation.y += event.movementX * rotationSpeed;
+    }
+});
+
+// Mouse up event
+window.addEventListener('mouseup', () => {
+    isDragging = false;
+    selectedCube = null;
+});
+
+
+        cube1.position.x = -2;
+        cube2.position.x = 2;
+
+        scene.add(cube1);
+        scene.add(cube2);
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+controls.enabled = false;
+
+        function onMouseClick(event) {
+            event.preventDefault();
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+            raycaster.setFromCamera(mouse, camera);
+            const intersects = raycaster.intersectObjects(scene.children);
+
+            if (intersects.length > 0) {
+                if (intersects[0].object === cube1) {
+                    window.location.href = "game-overview.html";
+                } else if (intersects[0].object === cube2) {
+                    window.location.href = "community-puzzles.html";
+                }
+            }
+        }
+
+        window.addEventListener('click', onMouseClick, false);
+
+        const animate = function () {
+            requestAnimationFrame(animate);
+            cube1.rotation.x += 0.01;
+            cube1.rotation.y += 0.01;
+            cube2.rotation.x += 0.01;
+            cube2.rotation.y += 0.01;
+            
+            renderer.render(scene, camera);
+        };
+  animate();
+};
+
+let isMouseDown = false;
+let previousMousePosition = { x: 0, y: 0 };
+
+window.addEventListener('mousedown', function() {
+    isMouseDown = true;
+    previousMousePosition = { x: event.clientX, y: event.clientY };
+});
+
+window.addEventListener('mouseup', function() {
+    isMouseDown = false;
+});
+
+window.addEventListener('mousemove', function(event) {
+    if (!isMouseDown) return;
+
+    const dx = event.clientX - previousMousePosition.x;
+    const dy = event.clientY - previousMousePosition.y;
+
+    const rotationSpeed = 0.005;
+
+    cube1.rotation.x += dy * rotationSpeed;
+    cube1.rotation.y += dx * rotationSpeed;
+    cube2.rotation.x += dy * rotationSpeed;
+    cube2.rotation.y += dx * rotationSpeed;
+
+    previousMousePosition = { x: event.clientX, y: event.clientY };
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('intro-animation').addEventListener('ended', function() {
+    // ... Your existing code ...
+    startThreeJsAnimation();
+  });
+});
--- main.js
+++ main.js
@@ -1,52 +1,169 @@
-import * as THREE from './lib/three.module.js'
-import { OrbitControls } from './lib/OrbitControls.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+window.startThreeJsAnimation = function() {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.z = 10;
+
+        const renderer = new THREE.WebGLRenderer( { alpha: true } );
+        renderer.setClearColor(0x000000, 0);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById('threejs-container').appendChild(renderer.domElement);
+
+        const textureLoader = new THREE.TextureLoader();
+        const texture = textureLoader.load('Assets/HomepagePlaceholder.png');
+        const material = new THREE.MeshBasicMaterial({ map: texture });
+
+        const geometry = new THREE.BoxGeometry();
+        const cube1 = new THREE.Mesh(geometry, material);
+        const cube2 = new THREE.Mesh(geometry, material);
+// Raycaster and mouse vector for selecting cubes
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+let selectedCube = null;
+let isDragging = false;
+
+// Mouse down event
+window.addEventListener('mousedown', (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects([cube1, cube2]);
+
+    if (intersects.length > 0) {
+        selectedCube = intersects[0].object;
+        isDragging = true;
+initiateSpin();
+    }
+});
+
+// Mouse move event
+window.addEventListener('mousemove', (event) => {
+    if (isDragging && selectedCube) {
+        const rotationSpeed = 0.01; // Adjust this value for desired rotation speed
+
+        selectedCube.rotation.x += event.movementY * rotationSpeed;
+        selectedCube.rotation.y += event.movementX * rotationSpeed;
+    }
+});
+
+// Mouse up event
+window.addEventListener('mouseup', () => {
+    isDragging = false;
+    selectedCube = null;
+});
 
 
-export function startThreeJsAnimation() {
-  const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xffffff); // White background
+        cube1.position.x = -2;
+        cube2.position.x = 2;
 
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 5;
-
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  // First cube
-  const geometry1 = new THREE.BoxGeometry();
-  const material1 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube1 = new THREE.Mesh(geometry1, material1);
-  scene.add(cube1);
-  cube1.position.x = -2; // Positioning the first cube to the left
-
-  // Second cube
-  const geometry2 = new THREE.BoxGeometry();
-  const material2 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  const cube2 = new THREE.Mesh(geometry2, material2);
-  scene.add(cube2);
-  cube2.position.x = 2; // Positioning the second cube to the right
-
-  // Orbit controls
-  const controls = new OrbitControls(camera, renderer.domElement);
-
-  const animate = function () {
-    requestAnimationFrame(animate);
-
-    cube1.rotation.x += 0.01;
-    cube1.rotation.y += 0.01;
-
-    cube2.rotation.x += 0.01;
-    cube2.rotation.y += 0.01;
-
-    controls.update();
-
-    renderer.render(scene, camera);
-  };
-
-  animate();
-
-  // Event listeners for cubes
-  // TODO: Add click event listeners to navigate to respective pages
+        scene.add(cube1);
+        scene.add(cube2);
+// Adjust cube positions based on screen width
+if (window.innerWidth < 600) {
+    // Position cubes vertically for mobile screens without any horizontal offset
+    cube2.position.set(0, cube1.position.y - 4.5, 0);
+} else {
+    // Position cubes horizontally for desktop screens
+    cube2.position.set(cube1.position.x + 4, 0, 0);
 }
 
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+controls.enabled = false;
+
+        function onMouseClick(event) {
+            event.preventDefault();
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+            raycaster.setFromCamera(mouse, camera);
+            const intersects = raycaster.intersectObjects(scene.children);
+
+            if (intersects.length > 0) {
+                if (intersects[0].object === cube1) {
+
+setTimeout(function() {
+    window.location.href = "game-overview.html";
+}, 2000);
+
+                } else if (intersects[0].object === cube2) {
+                    window.location.href = "community-puzzles.html";
+                }
+            }
+        }
+
+        window.addEventListener('click', onMouseClick, false);
+
+        const animate = function () {
+            requestAnimationFrame(animate);
+            cube1.rotation.x += 0.01;
+            cube1.rotation.y += 0.01;
+            cube2.rotation.x += 0.01;
+            cube2.rotation.y += 0.01;
+            
+            renderer.render(scene, camera);
+decelerateSpin(cube1);
+decelerateSpin(cube2);
+        };
+  animate();
+};
+
+let isMouseDown = false;
+let previousMousePosition = { x: 0, y: 0 };
+
+window.addEventListener('mousedown', function() {
+    isMouseDown = true;
+    previousMousePosition = { x: event.clientX, y: event.clientY };
+});
+
+window.addEventListener('mouseup', function() {
+    isMouseDown = false;
+});
+
+window.addEventListener('mousemove', function(event) {
+    if (!isMouseDown) return;
+
+    const dx = event.clientX - previousMousePosition.x;
+    const dy = event.clientY - previousMousePosition.y;
+
+    const rotationSpeed = 0.005;
+
+    cube1.rotation.x += dy * rotationSpeed;
+    cube1.rotation.y += dx * rotationSpeed;
+    cube2.rotation.x += dy * rotationSpeed;
+    cube2.rotation.y += dx * rotationSpeed;
+
+    previousMousePosition = { x: event.clientX, y: event.clientY };
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('intro-animation').addEventListener('ended', function() {
+    // ... Your existing code ...
+    startThreeJsAnimation();
+  });
+})
+let spinVelocity = 0;
+let isSpinning = false;
+
+function initiateSpin() {
+    spinVelocity = 0.15;  // Initial rotation speed
+    isSpinning = true;
+}
+
+function decelerateSpin(cube) {
+    if (isSpinning) {
+        spinVelocity *= 0.98;  // Deceleration factor
+
+        // Rotate the cube
+        cube.rotation.y += spinVelocity;
+
+        // If rotation is slow enough, snap to the nearest face
+        if (spinVelocity < 0.01) {
+            isSpinning = false;
+            cube.rotation.y = Math.round(cube.rotation.y / (Math.PI / 2)) * (Math.PI / 2);
+        }
+    }
+}
+;
